ef0db2c10f92e3ed8df9a68d02a2e5ff6f2ea369
conxadmin <mduduzi.keswa@conxsoft.com>
2016-09-13 12:18:56 -0400

    added docker compose

--- docker/gauntlet-backend/README.md
+++
@@ -1,7 +0,0 @@
-Project: conxworks/ace/run-client-xworkspace
-Bndrun: client-repo-copy.bndrun
-Generation steps: export .bndrun
-Jar name: conx-apache-ace-script-executor.jar
-
-Build:
-- scripts/build_and_run_local.sh
--- docker/gauntlet-backend/scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-# Make sure that we're setup with the docker machine correctly
-eval $(docker-machine env default)
-
-# Build
-docker build -t conxadmin/gauntlet-backend .
-docker push conxadmin/gauntlet-backend
